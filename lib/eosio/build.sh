259dd58b71d1e42ca0b3935d2e32e194371f8c7e
learnforpractice <learnforpractice@gmail.com>
2021-10-26 20:33:42 +0800

    Update eosio lib build script

--- lib/eosio/build.sh
+++ lib/eosio/build.sh
@@ -1,5 +1,13 @@
 mkdir -p build
 pushd build
-CXX=$(pwd)/../../../llvm-build/bin/clang C=$(pwd)/../../../llvm-build/bin/clang cmake .. -DLLVM_BUILD_DIR=$(pwd)/../../../llvm-build
+cmake .. -DLLVM_BUILD_DIR=$(pwd)/../../../llvm-build -DCMAKE_C_COMPILER_FORCED=TRUE -DCMAKE_CXX_COMPILER_FORCED=TRUE
 make -j4
 popd
+
+mkdir -p sysroot || exit 1
+
+mkdir -p sysroot/lib
+cp -r build/lib sysroot || exit 1
+
+mkdir -p sysroot/include || exit 1
+cp -r build/include/* sysroot/include
